ac4f071f2fec4cc8288221098b1b0c15632ba4f5
Nayan Vilas Pimpare <nayan.pimpare@yash.com>
2024-03-18 17:36:09 +0530

    Test case

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function App(props) {
+  // const logo = 1
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count === 5) {
+      console.log("Covered useEffect");
+    } else {
+      console.log("Else part to be covered");
+    }
+  }, [count]);
+
+  const handleClick = () => {
+    setCount((prevState) => {
+      return prevState + 1;
+    });
+  };
   return (
     <div className="App">
+      <h1>{props.fname}</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          data-testid="img"
+          alt="logo"
+          onClick={handleClick}
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/**
+ * @jest-environment jsdom
+ */
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import "@testing-library/jest-dom";
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App: ", () => {
+  test("renders learn react link", () => {
+    const { rerender, unmount } = render(<App {...{ fname: "Nayan" }} />);
+    const linkElement = screen.getByText(/learn react/i);
+
+    rerender(<App {...{ fname: "Nayan Pimpare" }} />);
+    expect(linkElement).toBeInTheDocument();
+    unmount();
+  });
+
+  test("should called handleClick function", () => {
+    const { container, unmount } = render(<App />);
+    let count = 0;
+    while (count < 5) {
+      fireEvent.click(screen.getByTestId("img"));
+      count++;
+    }
+
+    expect(container.querySelector(".App-link")).toBeInTheDocument();
+    unmount();
+  });
 });
