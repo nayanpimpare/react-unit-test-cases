4582d5f8405e63cff4320fc4a9dd918c76fd59a6
Nayan Vilas Pimpare <nayan.pimpare@yash.com>
2024-03-20 13:35:55 +0530

    Downgread coverage threshold.

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,12 @@ module.exports = {
   coveragePathIgnorePatterns: ["/node_modules/", ".*.svg"],
   collectCoverageFrom: ["src/**/*.js"],
   collectCoverage: false, // remove once tests are built out
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 80,
-  //     functions: 80,
-  //     lines: 80,
-  //     statements: 80,
-  //   },
-  // },  
+  coverageThreshold: {
+    global: {
+      branches: 10,
+      functions: 10,
+      lines: 10,
+      statements: 10,
+    },
+  },  
 };
