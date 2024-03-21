5c8a47dec381bf77961c11610ef8a7ea219a7c56
Nayan Vilas Pimpare <nayan.pimpare@yash.com>
2024-03-21 17:37:33 +0530

    jest config

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,10 @@ module.exports = {
   collectCoverage: false, // remove once tests are built out
   coverageThreshold: {
     global: {
-      branches: 10,
-      functions: 10,
-      lines: 10,
-      statements: 10,
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
     },
   },  
 };
